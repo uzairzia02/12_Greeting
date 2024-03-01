f39612ab4c7636d61e59f63591652ca2659a4ec1
uzairzia <uzair.governorsindh@gmail.com>
2024-03-01 08:57:04 +0500

    initial commit

---
+++ 12_Greetings.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = ['Uzair', 'Umair', 'Khan', 'Saad', 'Faisal'];
+for (let i = 0; i < names.length; i++) {
+    console.log(`Hi ${names[i]}, How are you?`);
+}
---
+++ 12_Greetings.ts
@@ -0,0 +1,5 @@
+let names: string[] = ['Uzair', 'Umair', 'Khan', 'Saad', 'Faisal'];
+
+for(let i=0; i<names.length; i++){
+    console.log(`Hi ${names[i]}, How are you?`);
+}
